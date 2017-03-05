32944553ecb23fd15a028614fd36089b592ebeb9
dev-felx <felixdevidf@gmail.com>
2017-03-05 20:34:00 +0300

    Export report is half working

--- application/views/event/reports_view.php
+++ application/views/event/reports_view.php
@@ -7,95 +7,193 @@
  */?>
 
 <div class="blank">
-    <div class="row">
-        <div class="col-md-6">
-            <div class="blank-page">
-                <div class="panel panel-danger">
-                    <div class="panel-heading">
-                        <div class="panel-title">
-                            <i class="glyphicon glyphicon-wrench pull-right"></i>
-                            <h4>Download Reports</h4>
-                        </div>
-                    </div>
 
 
-
-                    <div class="panel-body">
-
-                        <p> Below reports will help you to better analyse and understand your event very quick</p>
-                        <form class="form form-vertical">
-                            <div class="control-group">
-                                <label>Name</label>
-                                <div class="controls">
-                                    <input type="text" class="form-control" placeholder="Enter Name">
-                                </div>
-                            </div>
-                            <div class="control-group">
-                                <label>Message</label>
-                                <div class="controls">
-                                    <textarea class="form-control"></textarea>
-                                </div>
-                            </div>
-
-                    </div>
-                    <div class="control-group">
-                        <label></label>
-                        <div class="controls">
-                            <button type="submit" class="btn btn-primary">
-                                Create
-                            </button>
-                        </div>
-                    </div>
-                    </form>
-                </div>
-                <!--/panel content-->
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="blank-page">
-                <div class="panel panel-danger">
-                    <div class="panel-heading">
-                        <div class="panel-title">
-                            <i class="glyphicon glyphicon-wrench pull-right"></i>
-                            <h4>Create Custom Reports</h4>
-                        </div>
-                    </div>
-                    <div class="panel-body">
-
-                        <p>Using the form below, you may create and download a report of your choice for the category you select:</p>
+    <div class="blank-page col-md-8">
+        <h4>Reports</h4>
+        <hr/>
+        <table class="table">
+            <tbody>
+            <tr class="table-row">
+                <td class="">
+                     <i class="fa fa-users fa-3x"></i>
+                </td>
+                <td class="table-text">
+                    <h6>Members Report</h6>
+                    <p>A report that provides details on all members</p>
+                </td>
+                <td class="report_excel">
+                    <a href="javascript:void(0)" class="report fa fa-file-excel-o fa-2x" rel="<?php echo $event_id; ?>" id="members"></a>
+                    <p>Excel</p>
+                </td>
+                <td>
+                    <a class="fa fa-file-pdf-o fa-2x"></a>
+                    <p>PDF</p>
+                </td>
+            </tr>
 
 
-                        <div> <label class="" >Category</label>
-                            <select id="category-selection" class="" >
-                                <option value="0" selected="selected">Members</option>
-                                <option value="1">Budget</option>
-                                <option value="2">Eventy Type</option>
-                            </select>
-                        </div>
+            <tr class="table-row">
+                <td class="">
+                    <i class="fa fa-money fa-3x"></i>
+                </td>
+                <td class="table-text">
+                    <h6>Budget Report</h6>
+                    <p>A report that provides details on all budget items</p>
+                </td>
+                <td class="report_excel">
+                    <a href="javascript:void(0)" class="report fa fa-file-excel-o fa-2x" rel="<?php echo $event_id; ?>" id="budget"></a>
+                    <p>Excel</p>
+                </td>
+                <td>
+                    <a class="fa fa-file-pdf-o fa-2x"></a>
+                    <p>PDF</p>
+                </td>
 
-                        <form class="form form-vertical">
-                            <div class="control-group">
-                                <label>Name</label>
-                                <div class="controls">
-                                    <input type="text" class="form-control" placeholder="Enter Name">
-                                </div>
-                            </div>
+            </tr>
 
+            <tr class="table-row">
+                <td class="">
+                    <i class="fa fa-users fa-3x"></i>
+                </td>
+                <td class="table-text">
+                    <h6>Unpaid Pledges Report</h6>
+                    <p>A report that provides members' list who have paid less than the pledged amount</p>
+                </td>
+                <td class="report_excel">
+                    <a href="javascript:void(0)" class="report fa fa-file-excel-o fa-2x" rel="<?php echo $event_id; ?>" id="pledge"></a>
+                    <p>Excel</p>
+                </td>
+                <td>
+                    <a class="fa fa-file-pdf-o fa-2x"></a>
+                    <p>PDF</p>
+                </td>
 
-                    </div>
-                    <div class="control-group">
-                        <label></label>
-                        <div class="controls">
-                            <button type="submit" class="btn btn-primary">
-                                Create
-                            </button>
-                        </div>
-                    </div>
-                    </form>
-                </div>
-                <!--/panel content-->
-            </div>
-        </div>
+            </tr>
+
+            <tr class="table-row">
+                <td class="">
+                    <i class="fa fa-money fa-3x"></i>
+                </td>
+                <td class="table-text">
+                    <h6>Income vs Budget Report</h6>
+                    <p>A report that tells how much income collected against the proposed budget</p>
+                </td>
+                <td class="report_excel">
+                    <a href="javascript:void(0)" class="report fa fa-file-excel-o fa-2x" rel="<?php echo $event_id; ?>" id="income"></a>
+                    <p>Excel</p>
+                </td>
+                <td>
+                    <a class="fa fa-file-pdf-o fa-2x"></a>
+                    <p>PDF</p>
+                </td>
+
+            </tr>
+
+            <tr class="table-row">
+                <td class="">
+                    <i class="fa fa-info-circle fa-3x"></i>
+                </td>
+                <td class="table-text">
+                    <h6>Expenses vs Budget Report</h6>
+                    <p>A report that tells more about actual expenses done against the proposed budget</p>
+                </td>
+                <td class="report_excel">
+                    <a href="javascript:void(0)" class="report fa fa-file-excel-o fa-2x" rel="<?php echo $event_id; ?>" id="expenses"></a>
+                    <p>Excel</p>
+                </td>
+                <td>
+                    <a class="fa fa-file-pdf-o fa-2x"></a>
+                    <p>PDF</p>
+                </td>
+
+            </tr>
+            </tbody>
+        </table>
     </div>
-</div>
 
+    <div class="blank-page col-md-4">
+        <h4>Custom Reports</h4>
+        <hr/>
+        <p>You can create reports based on the categories defined below </p>
+
+
+        <div class="col-md-12 form-group2 group-mail">
+            <label class="control-label">Select Category</label>
+            <select>
+                <option value="0">Members</option>
+                <option value="1">Budget</option>
+                <option value="2">Event</option>
+                <option value="3">Contrary3</option>
+                <option value="4">Contrary4</option>
+            </select>
+        </div>
+
+        <div>
+
+            <label class="control-label">Create Report</label>
+            <div class="form-inline">
+
+                <div class="form-group">
+                    <a href="javascript:void(0)" class="report fa fa-file-excel-o fa-2x " rel="<?php echo $event_id; ?>" id="custom"></a>
+                    <p>Excel</p>
+                </div>
+
+                <div class="form-group">
+                    <a class="fa fa-file-pdf-o fa-2x"></a>
+                    <p>PDF</p>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+
+</div>
+<script>
+$(document).ready(function() {
+
+
+    var getReport = function(postData) {
+
+        $.ajax(
+            {
+                type:"POST",
+                url: "<?php echo base_url('event/reports')?>",
+                data:postData,
+                dataType: 'json',
+                success: function(data) {
+
+                    var $a = $("<a>");
+                    $a.attr("href",data.file);
+                    $("body").append($a);
+                    $a.attr("download",data.report_name);
+                    $a[0].click();
+                    $a.remove();
+
+                },
+
+                error: function(data) {
+
+                    alert('An error has occured while trying to get the report details, Please try again ');
+                }
+            });
+    }
+
+
+$('.report_excel').on("click", ".report", function() {
+var report_name = $(this).attr("id");
+var event_id = $(this).attr("rel");
+var postData = {
+'report_name': report_name,
+'event_id': event_id,
+};
+
+getReport(postData);
+
+});
+});
+</script>
