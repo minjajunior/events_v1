d93706240a67ddc67ea5d63c618ddc666d3dfc9d
dev-felx <felixdevidf@gmail.com>
2017-03-26 16:48:10 +0300

    PDF Report half completed .....Remove Excel report

--- application/views/reports/members_pdf.php
+++ application/views/reports/members_pdf.php
@@ -17,37 +17,41 @@ if(isset($this->session->admin_id)) {
 <div class="blank-page">
     <h4>Members</h4>
     <hr>
+
+
     <?php if (isset($member_details['error']) && $member_details['error'] == "0") { ?>
         No members found. Create <a href="#" data-toggle="modal" data-target="#newMember">new
             member</a> or upload your members file.
     <?php } else { ?>
-        <div class="tables">
-            <table class="table table-hover">
-                <thead>
-                <tr>
-                    <th>Member Name</th>
-                    <th>Pledge</th>
-                    <th>Cash</th>
-                    <th>Balance</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($member_details as $md) { ?>
-                    <tr>
-                        <?php if (isset($this->session->admin_id)) { ?>
-                            <td><a href="javascript:void(0)" class="edit_member" rel="<?php echo $md->member_id; ?>"
-                                   id="editMember_view"><?php echo $md->member_name ?></a></td>
-                        <?php } else { ?>
-                            <td><?php echo $md->member_name ?></td>
-                        <?php } ?>
-                        <td><?php echo $english_format_number = number_format($md->member_pledge, 0, '.', ',') . " Tsh."; ?></td>
-                        <td><?php echo $english_format_number = number_format($md->member_cash, 0, '.', ',') . " Tsh."; ?></td>
-                        <td><?php echo $english_format_number = number_format($md->member_pledge-$md->member_cash, 0, '.', ',') . " Tsh."; ?></td>
-                    </tr>
-                <?php } ?>
-                </tbody>
-            </table>
-        </div>
+
+    <div class="" data-example-id="">
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Member Name</th>
+                <th>Pledge</th>
+                <th>Cash</th>
+                <th>Balance</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $i=1;
+            foreach ($member_details as $md) { ?>
+            <tr>
+                <th scope="row"><?php echo $i; ?></th>
+                <td><?php echo $md->member_name ?></td>
+                <td><?php echo $english_format_number = number_format($md->member_pledge, 0, '.', ','); ?></td>
+                <td><?php echo $english_format_number = number_format($md->member_cash, 0, '.', ','); ?></td>
+                <td><?php echo $english_format_number = number_format($md->member_pledge-$md->member_cash, 0, '.', ','); ?></td>
+            </tr>
+            <?php $i++; } ?>
+
+            </tbody>
+        </table>
+    </div>
+
     <?php }
 
     }
