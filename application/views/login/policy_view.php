c13a640e06c31860b1c3606e2810be956a6cd38a
dev-felx <felixdevidf@gmail.com>
2017-05-27 22:17:12 +0300

    PDF reports completed

--- application/views/login/policy_view.php
+++ application/views/login/policy_view.php
@@ -23,44 +23,38 @@ $this->load->view('shared/login_header');
 
         <div class="questions">
             <h5>1.What personal information do we collect from the people that visit our website or app?</h5>
-            <p>When registering on our site, as appropriate, you may be asked to enter your name, email address, mailing address, phone number or other details to help you with your experience.</p>
+            <p>When registering on our site, as appropriate, you may be asked to enter your name, email address, phone number or other details to help you with your experience.</p>
         </div>
         <div class="questions">
             <h5>2.When do we collect information?</h5>
-            <p>We collect information from you when you register on our site, fill out a form or enter information on our site. Provide us with feedback on our products or services </p>
+            <p>We collect information from you when you register on our site, fill out a form or enter information on our site or provide us with feedback on our services </p>
         </div>
         <div class="questions">
             <h5>3.How do we use your information?</h5>
-            <p>We may use the information we collect from you when you register, sign up for our newsletter, respond to a survey or marketing communication, surf the website, or use certain other site features in the following ways:
+            <p>We may use the information we collect from you when you register, sign up for our newsletter, respond to a survey or marketing communication, surf the website, or use certain other site features in the following ways:<br/>
+            To personalize your experience and to allow us to deliver the type of content in which you are most interested.<br/>
+                To improve our website in order to better serve you.<br/>
+                To quickly process your transactions.</p>
 
-                • To personalize your experience and to allow us to deliver the type of content and product offerings in which you are most interested.
-                • To improve our website in order to better serve you.
-                • To allow us to better service you in responding to your customer service requests.
-                • To administer a contest, promotion, survey or other site feature.
-                • To quickly process your transactions.
-            </p>
         </div>
         <div class="questions">
             <h5>4.How do we protect your information?</h5>
-            <p>We do not use vulnerability scanning and/or scanning to PCI standards.
-                We only provide articles and information. We never ask for credit card numbers.
-                We do not use Malware Scanning.
+            <p>
+                We do not use Malware Scanning.<br/>
 
-                Your personal information is contained behind secured networks and is only accessible by a limited number of persons who have special access rights to such systems, and are required to keep the information confidential. In addition, all sensitive/credit information you supply is encrypted via Secure Socket Layer (SSL) technology.
+                Your personal information is contained behind secured networks and is only accessible by a limited number of persons who have special access rights to such systems, and are required to keep the information confidential. In addition, all sensitive/credit information you supply is encrypted via Secure Socket Layer (SSL) technology.<br/>
 
-                We implement a variety of security measures when a user enters, submits, or accesses their information to maintain the safety of your personal information.
+                We implement a variety of security measures when a user enters, submits, or accesses their information to maintain the safety of your personal information.<br/>
 
                 All transactions are processed through a gateway provider and are not stored or processed on our servers.</p>
         </div>
         <div class="questions">
             <h5>5.Do we use 'cookies'?</h5>
             <p>
-                Yes. Cookies are small files that a site or its service provider transfers to your computer's hard drive through your Web browser (if you allow) that enables the site's or service provider's systems to recognize your browser and capture and remember certain information. For instance, we use cookies to help us remember and process the items in your shopping cart. They are also used to help us understand your preferences based on previous or current site activity, which enables us to provide you with improved services. We also use cookies to help us compile aggregate data about site traffic and site interaction so that we can offer better site experiences and tools in the future.
-
-                We use cookies to:
-                • Understand and save user's preferences for future visits.
-                • Compile aggregate data about site traffic and site interactions in order to offer better site experiences and tools in the future. We may also use trusted third-party services that track this information on our behalf.
-
+                Yes. Cookies are small files that a site or its service provider transfers to your computer's hard drive through your Web browser (if you allow) that enables the site's or service provider's systems to recognize your browser and capture and remember certain information.
+                <br/>We use cookies to help us understand your preferences based on previous or current site activity, which enables us to provide you with improved services.
+                <br/>We also use cookies to help us compile aggregate data about site traffic and site interaction so that we can offer better site experiences and tools in the future.
+                <br/>
                 You can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies. You do this through your browser settings. Since browser is a little different, look at your browser's Help Menu to learn the correct way to modify your cookies.
             </p>
         </div>
--- application/views/login/terms_view.php
+++ application/views/login/terms_view.php
@@ -20,38 +20,62 @@ $this->load->view('shared/login_header');
         <!--//banner-->
         <!--faq-->
         <div class="asked">
-            <h4>Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before using the www.demievents.co.tz website (the "Service") operated by Demi Corporation Limited.
 
-                Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.
+            <div class="questions">
+                <p>Last updated: May 27, 2017</p>
+                <p>Please read these Terms and Conditions carefully before using the site (the "Service") operated by Demi Corporation Limited. Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.
+                    By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. </p>
+            </div>
+                <div class="questions">
+                <h5>Use of the site</h5>
+                    <p>
+                        You confirm that you are at least 18 years of age or are accessing the Site under the supervision of a parent or legal guardian. We grant you a non-transferable, revocable and non-exclusive license to use the Site, in accordance with the Terms and Conditions described herein, for the purpose of using services provided on the Site. Commercial use or use on behalf of any third party is prohibited, except as explicitly permitted by us in advance. Any breach of these Terms and Conditions shall result in the immediate revocation of the license granted in this paragraph without notice to you.
+                        Content provided on this site is solely for informational purposes. Service representations expressed on this Site are those of the vendors and are not made by us. Submissions or opinions expressed on this Site are those of the individual posting such content and may not reflect our opinions. Certain services and related features that may be made available on the Site may require registration or subscription. Should you choose to register or subscribe for any such services or related features, you agree to provide accurate and current information about yourself, and to promptly update such information if there are any changes.
+                        Every user of the Site is solely responsible for keeping passwords and other account identifiers safe and secure. The account owner is entirely responsible for all activities that occur under such password or account. Furthermore, you must notify us of any unauthorized use of your password or account. The Site shall not be responsible or liable, directly or indirectly, in any way for any loss or damage of any kind incurred as a result of, or in connection with, your failure to comply with this section.</p>
+            </div>
+            <div class="questions">
+                <h5>Liability for our Services</h5>
+                <p>WHEN PERMITTED BY LAW, DEMI WILL NOT BE RESPONSIBLE FOR LOST PROFITS, REVENUES, OR DATA, FINANCIAL LOSSES OR INDIRECT, SPECIAL, CONSEQUENTIAL, EXEMPLARY, OR PUNITIVE DAMAGES.
 
-                By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. This Terms & Conditions agreement is licensed by TermsFeed to Demi Events</h4>
-            <div class="questions">
-                <h5>1.What is Lorem Ipsum?</h5>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    TO THE EXTENT PERMITTED BY LAW, THE TOTAL LIABILITY OF DEMI, FOR ANY CLAIMS UNDER THESE TERMS, INCLUDING FOR ANY IMPLIED WARRANTIES, IS LIMITED TO THE AMOUNT YOU PAID US TO USE THE SERVICES (OR, IF WE CHOOSE, TO SUPPLYING YOU THE SERVICES AGAIN).
+
+                    IN ALL CASES, DEMI WILL NOT BE LIABLE FOR ANY LOSS OR DAMAGE THAT IS NOT REASONABLY FORESEEABLE.</p>
             </div>
             <div class="questions">
-                <h5>2.What is Lorem Ipsum dummy text ever since the 1500s?</h5>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h5>Trademarks and copyrights</h5>
+                <p>All intellectual property rights, whether registered or unregistered, in the Site, information content on the Site and all the website design, including, but not limited to, text, graphics, software, photos, video, music, sound, and their selection and arrangement, and all software compilations, underlying source code and software shall remain our property.
+                    The entire contents of the Site also are protected by copyright as a collective work under Tanzania copyright laws and international conventions. All rights are reserved.</p>
             </div>
             <div class="questions">
-                <h5>3.What is Duis laoreet, erat quis gravida porttitor odio felis ornare dui?</h5>
-                <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam semper elit risus, nec suscipit tellus tincidunt quis. Fusce non auctor enim. Suspendisse sit amet erat mollis, suscipit nisl sed, condimentum turpis. Curabitur faucibus, nisl ut aliquam porta, massa tortor tristique dolor, eget porttitor velit ligula vel eros. Sed egestas aliquet tellus. Nulla ac luctus urna. Pellentesque feugiat eros quis nibh consectetur, eget dapibus magna egestas. Phasellus non efficitur metus.</p>
+                <h5>Privacy</h5>
+                <p>
+                    We do not sell or rent your personal information to third parties for their marketing purposes without your explicit consent. We use your information only as described in the 'Demi Events' Privacy Policy. We view protection of users' privacy as a very important community principle. We store and process your information on computers that are protected by physical as well as technological security devices.
+                    You can access and modify the information you provide us and choose not to receive certain communications by signing-in to your account. We use third parties to verify and certify our privacy principles. For a complete description of how we use and protect your personal information, see the 'Demi Events' Privacy and Confidentiality Policy. If you object to your information being transferred or used in this way please do not use our services.
+
+                </p>
             </div>
             <div class="questions">
-                <h5>4.What is Quisque sollicitudin diam vel mauris volutpat viverra Proin ac imperdiet libero?</h5>
-                <p>Proin dignissim mi sit amet tincidunt varius. Quisque molestie fermentum dignissim. Sed in urna eget tortor congue tempus. Vestibulum gravida, erat in eleifend ultricies, felis lorem dictum nulla, ut tincidunt neque libero et nibh. Nullam at eros eu ligula auctor interdum a eget lorem. Praesent molestie eros vitae felis efficitur, at finibus sem molestie. Ut sit amet nisi at nunc pulvinar dapibus. Sed non neque et tortor finibus mattis. Mauris sit amet consectetur eros. Sed et ex posuere, vulputate dolor vel, pulvinar felis. </p>
+                <h5>Termination</h5>
+                <p>In addition to any other legal or equitable remedies, we may, without prior notice to you, immediately terminate the Terms and Conditions or revoke any or all of your rights granted under the Terms and Conditions. Upon any termination of this Agreement, you shall immediately cease all access to and use of the Site and we shall,
+                    in addition to any other legal or equitable remedies, immediately revoke all password(s) and account identification issued to you and deny your access to and use of this Site in whole or in part. Any termination of this agreement shall not affect the respective rights and obligations (including without limitation, payment obligations) of the parties arising before the date of termination.
+                    You furthermore agree that the Site shall not be liable to you or to any other person as a result of any such suspension or termination. If you are dissatisfied with the Site or with any terms, conditions, rules, policies, guidelines, or practices of Demi Corporation Limited in operating the Site, your sole and exclusive remedy is to discontinue using the Site.</p>
             </div>
             <div class="questions">
-                <h5>5.What is Nam semper elit risus, nec suscipit tellus tincidunt quis?</h5>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h5>Governing Law</h5>
+                <p>
+                    These Terms shall be governed and construed in accordance with the laws of United Republic of Tanzania, without regard to its conflict of law provisions.
+                    <br/>
+                    Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.
+
+                </p>
             </div>
             <div class="questions">
-                <h5>6.What is Quisque molestie fermentum dignissim?</h5>
-                <p>Sed eget nunc ex. Donec erat est, dapibus ac massa in, laoreet sagittis eros. Aliquam eget arcu feugiat leo elementum tristique. Praesent facilisis aliquam lorem sed pharetra. Donec scelerisque ipsum vel tincidunt gravida. Aliquam erat volutpat. Etiam mattis rhoncus massa, quis semper purus ornare id. Quisque sit amet mattis ipsum, in porttitor massa. Nullam tincidunt mi vel enim volutpat, a elementum libero imperdiet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean tincidunt eros eu nisl consequat facilisis. Maecenas ac accumsan elit, sit amet gravida nulla.</p>
-            </div>
-            <div class="questions">
-                <h5>7.What is Vestibulum ante ipsum primis in faucibus orci luctus et?</h5>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h5>About these Terms</h5>
+                <p> We may modify these terms or any additional terms that apply to a Service to, for example, reflect changes to the law or changes to our Services. You should look at the terms regularly. We’ll post notice of modifications to these terms on this page. We’ll post notice of modified additional terms in the applicable Service. Changes will not apply retroactively and will become effective no sooner than fourteen days after they are posted.
+                    However, changes addressing new functions for a Service or changes made for legal reasons will be effective immediately.
+                    If you do not agree to the modified terms for a Service, you should discontinue your use of that Service.
+
+                </p>
             </div>
 
         </div>
